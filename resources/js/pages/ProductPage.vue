29dbc9b098bd1107bb6261942c3a44bc0f90aadb
--replace-all <miqayelpetrosyan@gmail.com>
2023-09-17 22:47:53 +0400

    project install

---
+++ resources/js/pages/ProductPage.vue
@@ -0,0 +1,19 @@
+<template>
+    {{ product }}
+</template>
+
+<script>
+export default {
+    computed: {
+        product() {
+            return this.$store.getters.getProduct(this.$route.params.id)
+        }
+    },
+}
+
+</script>
+
+
+<style scoped>
+
+</style>

2482640e8b16c2ad2bb85534a4e94b8381fd8f91
miqay <miqayelpetrosyan@gmail.com>
2023-11-06 22:04:14 +0400

    Fe categpry menu fix

--- resources/js/pages/client/ProductPage.vue
+++ resources/js/pages/client/ProductPage.vue
@@ -26,8 +26,7 @@
 
             </div>
         </div>
-        <pre>{{ product.description }}</pre>
-        <div class="mt-16" v-html="product.description"/>
+        <pre class="mt-16">{{ product.description }}</pre>
         <div class="flex mt-20 gap-x-6">
             <Splide v-if="product.similars.length" :options="slider" aria-label="My Favorite Images"
                     class="w-full h-64">
